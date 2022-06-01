f219813b404583d3540fe5847703e0c10deb419f
DESKTOP-S2NT7V8\miho <b032fj@yamaguchi-u.ac.jp>
2022-06-01 14:23:31 +0900

    ファイルを追加しました

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+var num = 1;
+var elm = document.createElement("button");
+elm.innerHTML = num;
+elm.setAttribute("id", num);
+elm.setAttribute("class", "circle");
+elm.setAttribute("onclick", "remove()");
+document.getElementById("main").appendChild(elm);
+
+document.remove = function () {
+  document.getElementById("main").removeChild(document.getElementById("1"));
+};
